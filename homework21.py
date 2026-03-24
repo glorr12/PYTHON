9385ed37d5c7f25a8c1d6941131545d5b5135e36
glorr12 <igor.fishkov1991@gmail.com>
2026-03-24 23:43:48 +0000

    used Counter and defaultdict to solve tasks in homework21

--- homework21.py
+++ homework21.py
@@ -1,25 +1,20 @@
+from collections import Counter
 text = "Programming is fun!".lower()
+letter = [t for t in text if t.isalpha()]
+print(dict(Counter(letter)))
 
-def count_func(text):
-    new_dict = {}
-    for t in text:
-        if t.isalpha():
-            if t not in new_dict:
-                new_dict[t] = 1
-            else:
-                new_dict[t] += 1
-    return new_dict
-print(count_func(text))
+
 
 # ----------------------------------------------------
-
+from collections import defaultdict
 students = [("class1", "Alice"), ("class2", "Bob"), ("class1", "Charlie"), ("class3", "Daisy")]
 
 def group_students(data):
-    sort_group = {}
+    sort_group = defaultdict(list)
     for class_name,student_name in data:
-        if class_name not in sort_group:
-            sort_group[class_name] = []
         sort_group[class_name].append(student_name)
-    return sort_group
+    return dict(sort_group)
 print(group_students(students))
+
+
+
