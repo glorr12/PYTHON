0e0c89d4578f9fe7b7ab24bb2337755c3a99d516
glorr12 <igor.fishkov1991@gmail.com>
2026-04-02 13:55:52 +0100

    homework#25 solved first task added logs

--- homework25.py
+++ homework25.py
@@ -4,11 +4,8 @@ import logging
 logging.basicConfig(
  filename="errors.log",
  format="%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s -%(message)s",
- level=logging.DEBUG
+ level=logging.ERROR
 )
-logging.debug("Debug message")
-logging.info("Information message")
-logging.warning("Warning message")
 logging.error("Error")
 logging.critical("Critical error")
 def div():
@@ -20,7 +17,9 @@ def div():
         res = int(num) / int(num1)
         return res
     except ZeroDivisionError as d:
+        logging.critical("Ахнутг , гений делит на 0")
         return f"Нет нет нет, {d}"
     except ValueError:
+        logging.error("Алерт,алерт!Обнаружена строка в поле для интеджера")
         return "Ошибка.Введите корректные данные"
 print(div())
