4f3859ed25096217079ca7659d053ec97edb79e9
glorr12 <igor.fishkov1991@gmail.com>
2026-04-07 09:24:00 +0100

    fix .1

--- homework25.py
+++ homework25.py
@@ -3,18 +3,18 @@ import logging
 
 logging.basicConfig(
  filename="errors.log",
- format="%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s -%(message)s",encoding="utf-8",
+ format="%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s -%(message)s",encoding="utf-8",filemode="a",
  level=logging.ERROR
 )
 logging.error("Error")
 logging.critical("Critical error")
-def div():
+num1 = int(input("Введите делимое: "))
+num2 = int(input("Введите делитель: "))
+def div(num1, num2):
     try:
-        num = int(input("Введите делимое: "))
-        num1 = int(input("Введите делитель: "))
-        if int(num) == 0 or int(num1) == 0:
+        if int(num1) == 0 or int(num2) == 0:
             raise ZeroDivisionError("с нулём не работаем :)")
-        res = int(num) / int(num1)
+        res = int(num1) / int(num2)
         return res
     except ZeroDivisionError as d:
         logging.critical("Ахнутг , гений делит на 0")
@@ -22,4 +22,7 @@ def div():
     except ValueError:
         logging.error("Алерт,алерт!Обнаружена строка в поле для интеджера")
         return "Ошибка.Введите корректные данные"
-print(div())
+
+
+
+print(div(num1, num2))
