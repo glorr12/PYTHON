e58898925fe6b6514a874b2f2c466ae910727306
glorr12 <igor.fishkov1991@gmail.com>
2026-04-10 13:21:36 +0100

    fix .2

--- homework25.py
+++ homework25.py
@@ -1,28 +1,39 @@
-
 import logging
 
 logging.basicConfig(
- filename="errors.log",
- format="%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s -%(message)s",encoding="utf-8",filemode="a",
- level=logging.ERROR
+    filename="errors.log",
+    format="%(asctime)s - %(filename)s - %(lineno)d - %(levelname)s -%(message)s", encoding="utf-8", filemode="a",
+    level=logging.ERROR
 )
 logging.error("Error")
 logging.critical("Critical error")
-num1 = int(input("Введите делимое: "))
-num2 = int(input("Введите делитель: "))
-def div(num1, num2):
-    try:
-        if int(num1) == 0 or int(num2) == 0:
-            raise ZeroDivisionError("с нулём не работаем :)")
-        res = int(num1) / int(num2)
-        return res
-    except ZeroDivisionError as d:
-        logging.critical("Ахнутг , гений делит на 0")
-        return f"Нет нет нет, {d}"
-    except ValueError:
-        logging.error("Алерт,алерт!Обнаружена строка в поле для интеджера")
-        return "Ошибка.Введите корректные данные"
 
 
+def div(n1, n2) -> float:
+    """
+    Функция выполняет деление двух чисел с плавающей точкой.
+    :param n1: Это делимое. Переменная принимает как число так и строку.
+    :param n2: Это делитель. Переменная принимает как число так и строку.
+    :return: Результат деления.Выводится в виде числа с плавающей точкой.
+    """
+    n1 = float(n1)
+    n2 = float(n2)
+    if n1 == 0 or n2 == 0:
+        raise ZeroDivisionError("с нулём не работаем :)")
 
-print(div(num1, num2))
+    return n1 / n2
+
+
+num1 = input("Введите делимое: ")
+num2 = input("Введите делитель: ")
+
+try:
+    res = div(num1, num2)
+    print(f"Результат : {res}")
+
+except ZeroDivisionError as d:
+    logging.critical("Ахнутг , гений делит на 0")
+    print(f"Нет нет нет, {d}")
+except ValueError:
+    logging.error("Алерт,алерт!Обнаружена строка в поле для интеджера")
+    print("Ошибка.Введите корректные данные")
