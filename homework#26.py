5c6269040b514a71c980a7a6158ef1517b3215e6
glorr12 <igor.fishkov1991@gmail.com>
2026-04-10 14:05:02 +0100

    hw#26

---
+++ homework#26.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+finder = sys.argv[1]
+print(f"Содержимое директории : {finder}")
+print("Папки : ")
+for root, dirs, files in os.walk(finder):
+    for dir in dirs:
+        print(f"- {dir}\n")
+print("Файлы : ")
+for root, dirs, files in os.walk(finder):
+    for file in files:
+        print(f"- {file}\n")
+
+
+# ---------------------------------------------------------
+
+
+random = sys.argv[1]
+extension = sys.argv[2]
+
+for root, dirs, files in os.walk(random):
+    for file in files:
+        if file.endswith(extension):
+            print(f"Найден файл с расширением: '{extension}': ")
+            print(f"- {os.path.join(root, file)}")
+            while True:
+                delete_file = input("Вы хотите удалить эти файлы? (y/n): ").lower()
+                try:
+                    if delete_file not in ["y", "n"]:
+                        raise ValueError("Введите коррентные даннын типа y(да), n(нет)")
+                except ValueError as e:
+                    print(f"Найдена ошибка! {e}")
+                else:
+                    if delete_file == "y":
+                        os.remove(file)
+                        print("Файл удалён")
+                        break
+                    else:
+                        pass
+                        break
