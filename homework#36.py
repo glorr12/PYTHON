8ff662d1bf9e998742d656e9e4dd786806913c4a
glorr12 <igor.fishkov1991@gmail.com>
2026-05-08 15:06:26 +0100

    hw#36

---
+++ homework#36.py
@@ -0,0 +1,27 @@
+class Person:
+    def __init__(self, name:str)-> None:
+        self.name = name
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name}")
+
+class Student(Person):
+    def __init__(self, name:str, course:int)-> None:
+        super().__init__(name)
+        self.course = course
+    def introduce(self):
+        super().introduce()
+        print(f"I'm on course {self.course}")
+
+class Teacher(Person):
+    def __init__(self, name:str, subject:str)-> None:
+        super().__init__(name)
+        self.subject = subject
+
+    def introduce(self):
+        print(f"Hello, I am professor {self.name}. My subject is {self.subject}")
+
+eblani = [Student("Alice", 2), Teacher("Bob", "Mathematics")]
+
+for eblan in eblani:
+    eblan.introduce()
