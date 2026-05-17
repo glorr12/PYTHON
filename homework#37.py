c93604e1673834f65a5fe46dc3c714b89868a4c9
glorr12 <igor.fishkov1991@gmail.com>
2026-05-17 10:59:16 +0100

    reworked prints into strings

--- homework#37.py
+++ homework#37.py
@@ -2,20 +2,14 @@ class AudioFileMixin:
     def play_audio(self):
         if not hasattr(self, "audio_file"):
             raise AttributeError(f"{self.__class__.__name__} не найден audio_file")
-        print(f"Воспроизведение аудио для {self.__class__.__name__}")
-
-        for track in self.audio_file:
-            print(track)
+        return f"Воспроизведение аудио для {self.__class__.__name__}:\n{'\n'.join(self.audio_file)}"
 
 class VideoFileMixin:
     def play_video(self):
         if not hasattr(self, "video_file"):
             raise AttributeError(f"{self.__class__.__name__} не найден video_file")
 
-        print(f"Воспроизведение видео для {self.__class__.__name__}")
-
-        for video in self.video_file:
-            print(video)
+        return f"Воспроизведение видео для {self.__class__.__name__}:\n{'\n'.join(self.video_file)}"
 
 class MediaPlayer(AudioFileMixin):
     def __init__(self, track: list[str]):
@@ -29,7 +23,6 @@ class Laptop(AudioFileMixin, VideoFileMixin):
 tracks = ["track1.mp3", "track2.mp3"]
 movies = ["movie.mp4", "trailer.mov"]
 
-MediaPlayer(tracks).play_audio()
-
-Laptop(tracks, movies).play_video()
-Laptop(tracks, movies).play_audio()
+print(MediaPlayer(tracks).play_audio())
+print(Laptop(tracks,movies).play_audio())
+print(Laptop(tracks,movies).play_video())
